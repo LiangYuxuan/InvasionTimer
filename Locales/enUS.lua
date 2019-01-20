1f7cf557f55563ce78c6b4dfa5230ad75430f7ad
LiangYuxuan <lyx@lyx.es>
2019-01-20 17:47:42 +0800

    Fix enUS

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -3,7 +3,7 @@ local L = AceLocale:NewLocale("InvasionTimer", "enUS", true)
 if not L then return end
 -- enUS
 L["Invasion"] = "Invasion Timetable"
-L["BfA Invasion"] = "Faction Invasion"
+L["BfA Invasion"] = "Faction Assault"
 L["Legion Invasion"] = "Legion Invasion"
 L["Current Invasion"] = "Current: "
 L["Next Invasion"] = "Next: "
